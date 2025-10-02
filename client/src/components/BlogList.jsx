7c401fdf2be79ccb151ad0ae18bc9549bd061c40
Plitzee <122685269+Plitzee@users.noreply.github.com>
2025-10-02 18:20:09 +0700

    Thêm folder lên GitHub

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { blogCategories } from '../assets/assets';
-import { motion } from 'motion/react';
+import { blog_data, blogCategories } from '../assets/assets';
+import BlogCard from './BlogCard';
+
  
 const BlogList = () => {
 
@@ -15,17 +16,18 @@ const BlogList = () => {
             className ={`cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}>
                 {item}
                 {menu === item &&(
-                    <motion.div layoutID = 'underline'
-                    animate={{ x: 5 }}
-                    transition = {{type: 'spring', stiffness: 500, damping: 30}}
-                     className = 'absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'></motion.div>
+                    <div layoutID = 'underline'
+                    /*animate={{ x: 100 }}
+                    transition = {{type: 'spring', stiffness: 500, damping: 30}}"*/
+                     className = 'absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'></div>
                 )} 
             </button>
             </div>
         ))}
       </div>
-      <div>
-        {/* --- Blog cards --- */}
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40'>
+        {blog_data.filter((blog)=>menu === "All" ? true : blog.category === menu)
+        .map((blog)=><BlogCard key ={blog._id} blog = {blog}/>)}
       </div>
     </div>
   )
